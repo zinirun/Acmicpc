598b6ee3448c1375f7c2b9698157c8c77c9c8177
zinirun <heojj97@naver.com>
2020-05-08 20:05:32 +0900

    print A + B with Case#

---
+++ 11021.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int testCase;
+	int a, b;
+	
+	cin >> testCase;
+	
+	for(int i=0; i<testCase; i++){
+		cin >> a >> b;
+		cout << "Case #" << i+1 << ": " << a + b << endl;
+	}
+}
