df964b22d16fde39e23694c6b9aaa2f8188d6269
zinirun <heojj97@naver.com>
2020-05-11 09:30:46 +0900

    wine test #dp

---
+++ 2156.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int dp[10001], grape[10001];
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	for(int i=1; i<=n; i++)
+		scanf("%d", &grape[i]); 
+			
+	dp[1] = grape[1];
+	dp[2] = grape[1] + grape[2];
+	
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i-2] + grape[i]; //Áö±Ý ÀÜÀÌ Ã¹¹øÂ° ÀÜ
+		dp[i] = max(dp[i], dp[i-3] + grape[i-1] + grape[i]); //Áö±Ý ÀÜÀÌ µÎ¹øÂ° ÀÜ 
+		dp[i] = max(dp[i], dp[i-1]); //Áö±Ý ÀÜÀÌ ½¬¾ú´Ù °¡´Â ÀÜ 
+	}
+	
+	printf("%d\n", dp[n]);
+}
