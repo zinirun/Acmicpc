1ebfe866edab1d719c3a671fdce36eb2fe556a2e
zinirun <heojj97@naver.com>
2020-05-14 13:39:12 +0900

    ascent number #dp

---
+++ 11053.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int dp[1001], seq[1001], res=0;
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	for(int i=1; i<=n; i++)
+		scanf("%d", &seq[i]);
+	
+	for (int i = 1; i <= n; i++) {
+		dp[i] = 1;
+		for(int j = 0; j <= i; j++){
+			if(seq[j] < seq[i]){
+				dp[i] = max(dp[i], dp[j] + 1);
+				printf("%d: dpi %d dpj %d\n", i, dp[i], dp[j]);
+			}
+		}
+		res = max(res, dp[i]);
+	}
+	
+	printf("%d\n", res);
+}
