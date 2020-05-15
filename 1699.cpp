3584986f4ef77550fa21be0a7c99ec4d1754f3c3
zinirun <heojj97@naver.com>
2020-05-15 15:44:36 +0900

    sum of power number #dp

---
+++ 1699.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int dp[100001];
+int main() {
+    int n;
+    scanf("%d",&n);
+    
+    for(int i = 1; i <= n; i++) {
+    	dp[i] = i;
+        for(int j = 1; j*j <= i; j++) {
+            dp[i] = min(dp[i],dp[i-j*j] + 1);
+        }
+    }
+    printf("%d\n",dp[n]);
+    return 0;
+}
