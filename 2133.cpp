d229a78471a4b16f707848047b0b59d52f813fd0
zinirun <heojj97@naver.com>
2020-05-15 16:32:42 +0900

    tiling * 3 #dp

---
+++ 2133.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int dp[31] = {1, 0, 3};
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i = 4; i <= n; i+=2) {
+    	dp[i] = dp[i - 2] * 3;
+    	for(int j = 4; j <= i; j+=2){
+    		dp[i] += dp[i - j] * 2;
+		}
+    }
+    printf("%d\n", dp[n]);
+    return 0;
+}
